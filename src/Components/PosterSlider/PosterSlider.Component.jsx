e9707a715134e763f0032a7408be71cd03270c4b
gokul.b6411@gmail.com <gokul.6411@gmail.com>
2024-03-16 11:32:16 +0530

    new files created

--- src/Components/PosterSlider/PosterSlider.Component.jsx
+++ src/Components/PosterSlider/PosterSlider.Component.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import Slider from 'react-slick';
+import Poster from '../Poster/Poster.Components';
 
-const PosterSlider = () => {
+const PosterSlider = (props) => {
+  const {posters,title,subtitle,isdark} = props;
+  const settings = {};
   return (
-    <div>PosterSlider</div>
-  )
-}
+    <>
+    <div className='flex flex-col items-start sm:ml-3 my-2'>
+      <h3
+      className={`text-2xl font-bold ${
+        isdark ? "text-white":"text-black"
+      }`}
+      >
+        {title}
+      </h3>
+      <p className={`text-sm ${isdark ? "text-white" : "text-gray-800"}`}>
+        {subtitle}
+      </p>
+    </div>
+    <Slider {
+      ...settings
+    }>
+      {posters.map((each)=>(
+        <Poster {...each} isdark={isdark}/>
+      ))}
+    </Slider>
+    
+    
+    </>
+    
+  );
+};
 
 export default PosterSlider;
